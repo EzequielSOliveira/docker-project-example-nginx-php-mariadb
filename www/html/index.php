c3cebd1049cfd9b3109d17bc1d5251825e8fd353
Ezequiel da Silva Oliveira <95730033+EzequielSOliveira@users.noreply.github.com>
2022-11-13 09:32:25 -0500

    1

--- www/html/index.php
+++ www/html/index.php
@@ -9,11 +9,21 @@
     $host = getenv('MARIADB_HOST');
     $port = getenv('MARIADB_PORT');
     $dbname = getenv('MARIADB_DATABASE');
-    $user = getenv('MARIADB_USER');
-    $password = getenv('MARIADB_PASSWORD');
+    
+    if(empty(getenv('MARIADB_USER'))) {
+        $user = 'root';
+    } else {
+        $user = getenv('MARIADB_USER');
+    }
+    
+    if(empty(getenv('MARIADB_PASSWORD'))) {
+        $password = getenv('MARIADB_ROOT_PASSWORD');
+    } else {
+        $password = getenv('MARIADB_USER');
+    }
 
     try {
-        $dbh = new PDO('mysql:host=' . $host . ';port=' . $port . ';', $user, $password);
+        $dbh = new PDO('mysql:host=' . $host . ';port=' . $port . ';dbname=' . $dbname . ';', $user, $password);
         foreach($dbh->query('SHOW DATABASES;') as $row) {
             print_r($row);
         }
